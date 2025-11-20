545dfb27f386b72531cb7bcbf7225270eccfa38e
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-11-20 17:44:34 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "alb_dns" {
+  value = aws_lb.app.dns_name
+}
+
+output "ecr_repo_url" {
+  value = aws_ecr_repository.repo.repository_url
+}
+
+output "cluster_name" {
+  value = aws_ecs_cluster.cluster.name
+}
