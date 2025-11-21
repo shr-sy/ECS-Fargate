b8e95ef5e7953cff69c5ae31d59624c4550d3aa9
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-11-21 17:10:31 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -173,4 +173,87 @@ resource "aws_codebuild_project" "build" {
 # CodePipeline IAM Role
 ###########################
 resource "aws_iam_role" "pipeline_role" {
-  name = "${v
+  name = "${var.project_name}-pipeline-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "codepipeline.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "pipeline_policy" {
+  role = aws_iam_role.pipeline_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = ["s3:*", "codebuild:*", "iam:PassRole"]
+      Resource = "*"
+    }]
+  })
+}
+
+###########################
+# S3 Bucket for Pipeline
+###########################
+resource "aws_s3_bucket" "pipeline_bucket" {
+  bucket = "${var.project_name}-pipeline-artifacts"
+}
+
+###########################
+# CodePipeline
+###########################
+resource "aws_codepipeline" "pipeline" {
+  name     = "${var.project_name}-pipeline"
+  role_arn = aws_iam_role.pipeline_role.arn
+
+  artifact_store {
+    type     = "S3"
+    location = aws_s3_bucket.pipeline_bucket.bucket
+  }
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "GitHub_Source"
+      category         = "Source"
+      owner            = "ThirdParty"
+      provider         = "GitHub"
+      version          = "1"
+      output_artifacts = ["source_output"]
+
+      configuration = {
+        Owner      = var.github_owner
+        Repo       = var.github_repo
+        Branch     = var.github_branch
+        OAuthToken = var.github_oauth_token
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+
+    action {
+      name             = "Build"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      version          = "1"
+      input_artifacts  = ["source_output"]
+      output_artifacts = ["build_output"]
+
+      configuration = {
+        ProjectName = aws_codebuild_project.build.name
+      }
+    }
+  }
+}
