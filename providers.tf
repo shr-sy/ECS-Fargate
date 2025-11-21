9463a5558f5301eb93c1d7b3501f54b223770de0
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-11-21 12:48:55 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -21,7 +21,9 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region     = var.aws_region
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 provider "github" {
