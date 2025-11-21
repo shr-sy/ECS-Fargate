619a2df4cb0c108bb0c101c4fc6061b985e32e89
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-11-21 14:35:28 +0530

    Create outputs.tf

---
+++ terraform/outputs.tf
@@ -0,0 +1,11 @@
+output "alb_dns" {
+  value = aws_lb.alb.dns_name
+}
+
+output "ecr_repo_url" {
+  value = aws_ecr_repository.app.repository_url
+}
+
+output "cluster_name" {
+  value = aws_ecs_cluster.cluster.name
+}
