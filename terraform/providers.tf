14b471e2535da37d2c9880b28fd1ddc6c2a43aa3
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-11-21 16:39:34 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,11 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.26"
+      version = "5.26.0"
     }
   }
-}
-
-provider "aws" {
-  region = var.aws_region
 }
