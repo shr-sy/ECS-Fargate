44dc4aac3f219bc8c859275efb0b50b4fe9e3936
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-11-21 15:27:35 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,19 @@
-output "alb_dns" {
-  value = aws_lb.alb.dns_name
+output "ecr_repo_url" {
+  description = "ECR repository URL"
+  value       = module.ecr.repository_url
 }
 
-output "ecr_repo_url" {
-  value = aws_ecr_repository.app.repository_url
+output "alb_dns_name" {
+  description = "Load Balancer DNS Name"
+  value       = module.alb.lb_dns_name
 }
 
 output "cluster_name" {
-  value = aws_ecs_cluster.cluster.name
+  description = "ECS Cluster Name"
+  value       = module.ecs.cluster_name
+}
+
+output "service_name" {
+  description = "ECS Service Name"
+  value       = module.ecs.service_name
 }
