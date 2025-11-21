b972b6ba66b8fcd5a70cd5ee4d0a05c4afbb3909
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-11-21 12:26:56 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.5"
+  required_version = ">= 1.3.0"
 
   cloud {
-    organization = "your-hcp-org"
+    organization = "YOUR_HCP_ORG"
     workspaces {
-      name = "ecs-fargate-ws"
+      name = "YOUR_WORKSPACE"
     }
   }
 
@@ -13,11 +13,18 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
   }
 }
 
 provider "aws" {
-  region     = var.aws_region
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
+  region = "us-east-1"
+}
+
+provider "github" {
+  token = var.github_oauth_token
+  owner = var.github_owner
 }
