53fb67dc4121945322b02477e788d52fc582696f
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-11-21 14:53:34 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,17 @@
 variable "aws_region" {
-  description = "AWS region"
+  description = "AWS region to deploy resources"
   type        = string
   default     = "us-east-1"
 }
 
+variable "project_name" {
+  description = "Base name for all resources"
+  type        = string
+  default     = "efp"
+}
+
 variable "github_owner" {
-  description = "GitHub owner/org"
+  description = "GitHub organization or username"
   type        = string
 }
 
@@ -15,40 +21,67 @@ variable "github_repo" {
 }
 
 variable "github_branch" {
-  description = "GitHub branch for pipeline"
+  description = "GitHub branch to trigger CodePipeline"
   type        = string
   default     = "main"
 }
 
-variable "project_name" {
-  description = "Base project name"
-  type        = string
-  default     = "efp"
-}
-
 variable "vpc_cidr" {
-  description = "VPC CIDR"
+  description = "CIDR block for VPC"
   type        = string
   default     = "10.0.0.0/16"
 }
 
+variable "public_subnet_cidrs" {
+  description = "Public subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "Private subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
 variable "container_port" {
-  description = "Container port"
+  description = "Application port exposed by the container"
   type        = number
   default     = 3000
 }
 
 variable "cpu" {
-  type    = number
-  default = 256
+  description = "Fargate task CPU"
+  type        = number
+  default     = 256
 }
 
 variable "memory" {
-  type    = number
-  default = 512
+  description = "Fargate task memory"
+  type        = number
+  default     = 512
 }
 
 variable "desired_count" {
-  type    = number
-  default = 1
+  description = "Number of ECS task replicas"
+  type        = number
+  default     = 1
+}
+
+variable "environment" {
+  description = "Environment name (dev, qa, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "codebuild_compute_type" {
+  description = "Build compute size"
+  type        = string
+  default     = "BUILD_GENERAL1_SMALL"
+}
+
+variable "codebuild_image" {
+  description = "CodeBuild environment image"
+  type        = string
+  default     = "aws/codebuild/standard:7.0"
 }
