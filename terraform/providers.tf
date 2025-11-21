453899abdd5e3973fcc6d5316146aa563dfad04c
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-11-21 15:47:20 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,14 +1,37 @@
+############################################################
+# TERRAFORM & PROVIDERS
+############################################################
 terraform {
   required_version = ">= 1.3.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.69"  # Latest 5.x stable version
+      version = "~> 5.69"  # Pin to latest stable 5.x version
+    }
+  }
+
+  backend "remote" {
+    organization = "YOUR_HCP_ORG"
+
+    workspaces {
+      name = "YOUR_WORKSPACE"
     }
   }
 }
 
+############################################################
+# AWS PROVIDER
+############################################################
 provider "aws" {
+  region = var.aws_region
+}
+
+############################################################
+# OPTIONAL: Pin all modules to use this provider
+# Ensures no conflicts with module constraints
+############################################################
+provider "aws" {
+  alias  = "default"
   region = var.aws_region
 }
