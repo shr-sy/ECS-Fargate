804378e049f1f571a73beeee5ce2ecb6cb17b6f2
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-11-21 15:34:05 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,19 +1,15 @@
 output "ecr_repo_url" {
-  description = "ECR repository URL"
-  value       = module.ecr.repository_url
+  value = module.ecr.repository_url
 }
 
 output "alb_dns_name" {
-  description = "Load Balancer DNS Name"
-  value       = module.alb.lb_dns_name
+  value = module.alb.lb_dns_name
 }
 
 output "cluster_name" {
-  description = "ECS Cluster Name"
-  value       = module.ecs.cluster_name
+  value = module.ecs.cluster_name
 }
 
 output "service_name" {
-  description = "ECS Service Name"
-  value       = module.ecs.service_name
+  value = module.ecs_service.service_name
 }
